62471629cba0f4b383e430872a6d93568468ed36
DonatasTamosauskas <donatas.tamosauskas@gmail.com>
2019-06-09 19:52:04 +0000

    Updates to dockefile and additional softmax layer for type predictor

--- classifiers/type_classifier.py
+++ classifiers/type_classifier.py
@@ -1,9 +1,13 @@
 from io import BytesIO
 from PIL import Image
 from torchvision import transforms
+from torch.nn import Sequential, Softmax
 from fastai.vision import load_learner
 
-LEARNER_PKL_LOC = "data/"
+import os
+
+LEARNER_PKL_LOC = "models/"
+LEARNER_PKL_NAME = "TypeClass_ResNet50.pkl"
 
 # Need to install:
 #  Pillow
@@ -23,33 +27,34 @@ class TypeClassifier:
     """Clothing image classifier that uses pytorch CNN model trained with Fast.ai library."""
     
     def __init__(self, img_size=224):
-      self.img_size = img_size
-      
-      self.transform = transforms.Compose([
-          transforms.Resize((self.img_size, self.img_size)),
-          transforms.ToTensor(),
-          transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                               std=[0.229, 0.224, 0.225])
-      ])
-      
-      self.model = self._get_model()
+        self.img_size = img_size
+        
+        self.transform = transforms.Compose([
+            transforms.Resize((self.img_size, self.img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+        ])
+        
+        self.model = self._get_model()
 
     def classify(self, file):
         """Classify given image stream into one of clothing type categories. Return a dict of probabilites"""
         preped_img = self._prep_image(file)
-        # Might need to alter dimensions to signal batch of one entity
         predictions = self.model(preped_img)
         return predictions
       
     def _prep_image(self, image):
-      """Prepare image for inference by resizing, converting to tensor and normalizing"""
-      img = Image.open(BytesIO(image))
-      tensor = self.transform(img)
-      return tensor[None, :, :, :]
-    
+        """Prepare image for inference by resizing, converting to tensor and normalizing"""
+        img = Image.open(image)
+        tensor = self.transform(img)
+        return tensor[None, :, :, :]
+        
     def _get_model(self):
-      learner = load_learner(LEARNER_PKL_LOC)
-      model = learner.model
-      model.training = False
-      model.cpu()
-      return model
+        learner = load_learner(LEARNER_PKL_LOC, LEARNER_PKL_NAME)
+        self.classes = learner.data.classes
+        model = learner.model
+        model.training = False
+        model.cpu()
+        model = Sequential(model, Softmax(1))
+        return model
--- clothing_classifier.py
+++ clothing_classifier.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from classifiers.type_classifier import TypeClassifier
 
 app = Flask(__name__)
@@ -8,9 +8,23 @@ cat_classifier = TypeClassifier()
 @app.route('/category', methods=['GET', 'POST'])
 def classify_clothing_type():
     """Classify given image into one of clothing categories"""
-    image_file = request.files['image']
+    image_file = _get_image_from_request(request)
 
     predictions = cat_classifier.classify(image_file)
     print(predictions)
-    return "Testy"
+    return _format_response("Placeholder predicions")
+
+def _get_image_from_request(received_req):
+    if (received_req.files is not None 
+        and 'image' in received_req.files.keys()):
+        return request.files['image'].stream._file
+
+    # TODO: Add different image sending handling
+
+def _format_response(predictions):
+    # TODO: Create a JSON reply
+    return make_response(predictions)
+    
+
+
 
