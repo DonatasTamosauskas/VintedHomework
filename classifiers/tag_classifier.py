107d894482f2798ff2f89d3e58ede0b703fa7c01
DonatasTamosauskas <donatas.tamosauskas@gmail.com>
2019-06-10 12:55:19 +0000

    Created tag classifier and moved some functionality from classifiers to utils

--- classifiers/tag_classifier.py
+++ classifiers/tag_classifier.py
@@ -3,28 +3,24 @@ from classifiers.utils import get_model_and_classes, prep_image
 
 
 LEARNER_PKL_LOC = "models/"
-LEARNER_PKL_NAME = "TypeClass_ResNet50.pkl"
+LEARNER_PKL_NAME = "TagClass_ResNet50.pkl"
 
-# Also, for lighter docker image use the CPU ONLY version of pytorch
-# and find a way to remove text corpuses from fastai
 
-class TypeClassifier:
-    """Clothing type from image classifier that uses pytorch CNN model trained with Fast.ai library."""
-    
+class TagClassifier:
+    """Chlothing material pattern classifier that uses pytorch CNN model trained with Fast.ai library."""
+
     def __init__(self, img_size=224):
         self.img_size = img_size
-        
         self.transform = transforms.Compose([
             transforms.Resize((self.img_size, self.img_size)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225])
         ])
-        
         self.model, self.classes = get_model_and_classes(LEARNER_PKL_LOC, LEARNER_PKL_NAME)
 
     def classify(self, file):
-        """Classify given image stream into one of clothing type categories. Return a dict of probabilites"""
+        """Classify given image stream into one of pattern categories. Return a dict of probabilites"""
         preped_img = prep_image(file, self.transform)
         prediction_tensor = self.model(preped_img)
         predictions = prediction_tensor.data.cpu().numpy()[0].tolist()
---
+++ classifiers/utils.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from torch.nn import Sequential, Softmax
+from fastai.vision import load_learner
+
+
+def prep_image(image, transforms):
+    """Prepare image for inference by resizing, converting to tensor and normalizing."""
+    img = Image.open(image)
+    tensor = transforms(img)
+    return tensor[None, :, :, :]
+    
+def get_model_and_classes(pkl_location, pkl_name):
+    """Get the trained pytorch model and classes.""" 
+    learner = load_learner(pkl_location, pkl_name)
+    classes = learner.data.classes
+    model = learner.model
+    model.training = False
+    model.cpu()
+    model = Sequential(model, Softmax(1))
+    return model, classes
